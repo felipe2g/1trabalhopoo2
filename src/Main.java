2e0b79e5c413c1fdaa2ef9620793d212f7511544
Felipe Guimarães <54296382+felipe2g@users.noreply.github.com>
2023-03-03 21:36:04 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -6,28 +6,23 @@ import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
-//        Paciente
-//        Medico
-//        Consultas
-        Pacient pacient1 = new Pacient("João JQuery", new Date());
-        Doctor doctor1 = new Doctor("Doctor AJAX", "111111", "Consulta de rotas");
-        Pacient pacient2 = new Pacient("Maluco do Bradesco", new Date());
+        Pacient pacient1 = new Pacient("Pessoa 1", new Date());
+        Doctor doctor1 = new Doctor("Doutor 1", "111111", "Endocrinologia");
+        Pacient pacient2 = new Pacient("Paciente 2", new Date());
 
         DoctorsAppointment consulta1 = new DoctorsAppointment(doctor1, pacient1, new Date());
         DoctorsAppointment consulta2 = new DoctorsAppointment(doctor1, pacient2, new Date());
 
         System.out.println(consulta1);
 
-        consulta1.cancelAppointment("Pinéu da cabeça com as rotas");
+        consulta1.cancelAppointment("Dor de cabeça");
 
         System.out.println(consulta1);
 
         System.out.println(consulta2);
 
-        consulta2.finishAppoitment("O cara estava maluco fazendo rotas o dia inteiro");
+        consulta2.finishAppoitment("Estava trabalhando muito");
 
         System.out.println(consulta2);
-
-
     }
 }
